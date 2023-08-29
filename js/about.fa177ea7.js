7a91ed8793955e132062310669ffc1f0c231bf8b
Ervin Ungvari <ungvari.ervin@yahoo.com>
2023-08-29 14:28:56 +0300

    a

---
+++ js/about.fa177ea7.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunksave_money"]=self["webpackChunksave_money"]||[]).push([[443],{653:function(n,e,s){s.r(e),s.d(e,{default:function(){return i}});var t=function(){var n=this;n._self._c;return n._m(0)},u=[function(){var n=this,e=n._self._c;return e("div",{staticClass:"about"},[e("h1",[n._v("This is an about page")])])}],a=s(1001),l={},r=(0,a.Z)(l,t,u,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about.fa177ea7.js.map
